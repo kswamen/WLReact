da637120c71c95b920f8a8a75140b4f688865b1c
KimSeokWon <kswamen@naver.com>
2021-01-28 22:43:44 +0900

    210128

---
+++ client/build/precache-manifest.1590d9249ffecf2db5cc5fd391a1a93e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695793eb09fb68f92508ff2ddcfc677e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804d0302c192079a27ca",
+    "url": "/static/css/2.832d9ecb.chunk.css"
+  },
+  {
+    "revision": "109bc5272baa5ac02448",
+    "url": "/static/css/main.27df9b2e.chunk.css"
+  },
+  {
+    "revision": "804d0302c192079a27ca",
+    "url": "/static/js/2.1ef8f000.chunk.js"
+  },
+  {
+    "revision": "02d17cad1987f37a156bd765b1acb693",
+    "url": "/static/js/2.1ef8f000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "109bc5272baa5ac02448",
+    "url": "/static/js/main.ccee3d6d.chunk.js"
+  },
+  {
+    "revision": "51a8196ef52c5028c970",
+    "url": "/static/js/runtime-main.43d603c7.js"
+  },
+  {
+    "revision": "3646e1009295f29839b16fcbf6de7c18",
+    "url": "/static/media/CoronaIcon.3646e100.png"
+  },
+  {
+    "revision": "e991dea9f65ce799dd72095ab7ad6e88",
+    "url": "/static/media/Oswald-Bold.e991dea9.ttf"
+  },
+  {
+    "revision": "ade38d2d4198e85512f66b5b930c3d45",
+    "url": "/static/media/bg1.ade38d2d.jpeg"
+  },
+  {
+    "revision": "4a93bdb8ef02361538d329c62d7a4cb1",
+    "url": "/static/media/bg2.4a93bdb8.jpg"
+  },
+  {
+    "revision": "6c3c90ccf08567b1c7c0b4c060dd88af",
+    "url": "/static/media/bg3.6c3c90cc.jpg"
+  },
+  {
+    "revision": "f33975f715a88e97f4ffba388347bdf2",
+    "url": "/static/media/title.f33975f7.png"
+  }
+]);
